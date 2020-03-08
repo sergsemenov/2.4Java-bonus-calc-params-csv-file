119e2797392b158b1ddd14643802440ee70da97b
Sergey Semenov <gzuppy@gmail.com>
2020-03-08 18:24:30 +0300

    --amend

--- src/test/java/ru/netology/bonus/BonusService.java
+++ src/test/java/ru/netology/bonus/BonusService.java
@@ -1,6 +1,7 @@
 package ru.netology.bonus;
 
 public class BonusService {
+
     public long calculate(long amount, boolean registered) {
         int percent = registered ? 3 : 1;
         long bonus = amount * percent / 100 / 100;
