681d7b498fb6a93c781873f19f882db78acfe9c0
Sergey Semenov <gzuppy@gmail.com>
2020-03-08 18:21:27 +0300

    Code reformatted, import improved

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -3,12 +3,12 @@ package ru.netology.bonus;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
+
     @ParameterizedTest
     @CsvFileSource(resources = "/data.csv")
-
     void shouldCalculate(String test, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
 
